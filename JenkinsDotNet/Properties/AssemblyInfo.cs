2a867d1db79b1e05c1d05421005c0b11bf988a6a
alitheg <alastair@alastairmcfarlane.co.uk>
2013-06-10 22:48:29 +0100

    Updating AssemblyInfo.cs

--- JenkinsDotNet/Properties/AssemblyInfo.cs
+++ JenkinsDotNet/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Jenkins.NET")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(".NET wrapper for Jenkins API")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Alastair McFarlane")]
 [assembly: AssemblyProduct("Jenkins.NET")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © Alastair McFarlane 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
